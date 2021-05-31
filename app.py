e2f16e1490ea2ccadadb93014e0aacf676000236
Tanojsaipraveen <47939411+tanojsaipraveen@users.noreply.github.com>
2021-05-31 14:09:05 +0530

    updateddd

--- app.py
+++ app.py
@@ -25,21 +25,13 @@ class TrendingSongs(Resource):
         response = requests.get("https://www.jiosaavn.com/api.php?__call=webapi.getLaunchData&api_version=4&_format=json&_marker=0&ctx=wap6dot0",headers={"accept-language":"en-US,en;q=0.9,te-IN;q=0.8,te;q=0.7"})
         return response.json()
     
-class TorrentbyName(Resource):
-    def get(self,name):
-        response = requests.get("https://api-torrent.vercel.app/api/v1/search?query="+name+"&key=U2FsdGVkX1+n6JlCR3BgA/NkTFI1fWA1rBpg1J+qBHBx2aWt1ZxwHvN0kQXRIjoN")
-        return response.json()
-    
-class TorrentbyId(Resource):
-    def get(self,id):
-        response = requests.get("https://api-torrent.vercel.app/api/v1/detail/"+id+"?key=U2FsdGVkX1+n6JlCR3BgA/NkTFI1fWA1rBpg1J+qBHBx2aWt1ZxwHvN0kQXRIjoN"")
-        return response.json()
+
 
 
 api.add_resource(HelloWorld,"/helloworld/<string:name>")
-api.add_resource(TorrentbyName,"/torrentbyname/<string:name>")
+
 api.add_resource(SongDetails,"/songdetails/<string:id>")
-api.add_resource(TorrentbyId,"/torrentbyid/<string:id>")
+
 api.add_resource(TrendingSongs,"/trendingsongs/")
 
 
