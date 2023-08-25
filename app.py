4c183caa7200651aea4a53583b2c56e38c2c7cd5
Tanojsaipraveen <47939411+tanojsaipraveen@users.noreply.github.com>
2023-08-25 19:22:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ class HelloWorld(Resource):
     def get(self,name):
         response = requests.get("https://www.jiosaavn.com/api.php?p=1&q="+name+"&_format=json&_marker=0&api_version=4&ctx=web6dot0&n=20&__call=search.getAlbumResults")
         return response.json()
+
+class AppVersion(Resource):
+    def get(self,name):
+        return "1.0.0"
     
 class GetMovieData(Resource):
     def get(self,name):
